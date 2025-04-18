32980940bed15fd2bb6c222239d5f0114da9ef1b
SantiagoDopazo <104122457+SantiagoDopazo@users.noreply.github.com>
2025-04-18 16:56:22 -0300

    fix: tabs to size 4

--- app/controllers/healthCheckController.js
+++ app/controllers/healthCheckController.js
@@ -1,8 +1,8 @@
 export class HealthCheckController {
-  check(req, res) {
-    res.status(200).json({
-      status: 'ok',
-      message: 'Birbnb Api is up'
-    });
-  }
+    check(req, res) {
+        res.status(200).json({
+            status: 'ok',
+            message: 'Birbnb Api is up'
+        });
+    }
 }
--- app/routes/healthCheckRoutes.js
+++ app/routes/healthCheckRoutes.js
@@ -1,7 +1,7 @@
 import { HealthCheckController } from "../controllers/healthCheckController.js";
 
 export function healthCheckRoutes(app, getController) {
-  app.get("/health_check", (req, res) => {
-    getController(HealthCheckController).check(req, res);
-  });
+    app.get("/health_check", (req, res) => {
+        getController(HealthCheckController).check(req, res);
+    });
 }
